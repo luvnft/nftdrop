6617e3c0735bd3ff39711b7a16e3fff44ac1a79d
Reko <reko.kalkaja@gmail.com>
2024-06-22 23:36:52 +0300

    init api and web ui

---
+++ web/src/routes/admin/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { createQrSvgString, createQrSvgDataUrl } from '@svelte-put/qr';
+
+	const config = { data: 'http://192.168.10.94:5173/projects?id=moi' };
+
+	const dataURL = createQrSvgDataUrl(config);
+	const svgString = createQrSvgString(config);
+</script>
+
+<div
+	style:display="flex"
+	style:flex-direction="column"
+	style:align-items="center"
+	style:justify-items="center"
+	style:height="100vh"
+>
+	<div style:width="50vw" style:max-width="75vh" style:max-height="70vh">
+		{@html svgString}
+	</div>
+
+	<a style="margin: 40px" class="c-btn" href={dataURL} download="qr.svg">Download QR as SVG</a>
+</div>
---
+++ web/src/routes/projects/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { page } from '$app/stores'
+  const projectId = $page.url.searchParams.get('id')
+  console.log(projectId)
+</script>
+
+<h1>You're getting a free NFT from {projectId}!</h1>
+<p>Please identify yourself to mint your unique collectible!</p>
